9d250e5bfd58c3cfa0eabe8df14b702163b446cc
Alessio <alessio.rocchi@iit.it>
2016-07-27 15:38:03 +0200

    New reflex emulator code runs. Need refactor, and transmission matrix check (weird behavior at the flex joint ATM)

--- plugins/reflex.py
+++ plugins/reflex.py
@@ -18,7 +18,7 @@ numLinks = 19
 numCommandDims = 4
 
 #The names of the command dimensions
-commandNames = ['synergy']
+commandNames = ['finger1','finger2','finger3','preshape']
 
 #default postures
 openCommand = [1]
@@ -36,6 +36,10 @@ commandMaximum = [1]
 #range of valid command velocities
 commandMinimumVelocity = [-1]
 commandMaximumVelocity = [1]
+
+swivel_links = [2,7]
+proximal_links = [3,8,12]
+distal_links = [4,9,13]
 
 class HandModel:
     """A kinematic model of the Reflex hand"""
@@ -78,6 +82,9 @@ class HandEmulator(ActuatorEmulator):
         self.mimic = dict()
         self.n_dofs = 0
 
+        self.n_fingers = 3
+        self.u_dofs_per_finger = 2
+
         self.K_p = 3.0
         self.K_d = 0.03
         self.K_i = 0.01
@@ -92,7 +99,8 @@ class HandEmulator(ActuatorEmulator):
         print "Number of Drivers:", self.robot.numDrivers()
         if self.robot.getName() in [gripper_name, "temp"]:
             self.n_dofs = self.robot.numDrivers()
-            self.a_dofs = numCommandDims
+            self.a_dofs = numCommandDims - 1
+            self.d_dofs = 1
             self.m_dofs = 0
             self.u_dofs = 6
         else:
@@ -109,34 +117,50 @@ class HandEmulator(ActuatorEmulator):
         self.n_to_m = np.array(self.n_dofs*[-1])
         self.n_to_a = np.array(self.n_dofs*[-1])
         self.n_to_d = np.array(self.n_dofs*[-1])
-        # skip the fixed joints:
-        # - soft_hand_kuka_coupler
-        # - soft_hand_clamp
-        # - soft_hand_softhand_base
-        # - soft_hand_palm_link
-        self.q_to_t = np.array(self.n_dofs)
+
+        self.q_to_t = []    # maps active drivers to joint ids
+                            # (basically removes weld joints, counts affine joints only once, takes into account
+                            #  floating base and regular joints properly)
+
+        for i in xrange(driver_offset):
+            driver = self.robot.driver(i)
+            link = self.robot.link(driver.getName())
+            self.q_to_t.append(link.getIndex())
 
         # loading previously defined maps
         for i in xrange(driver_offset, self.robot.numDrivers()):
             driver = self.robot.driver(i)
             link = self.robot.link(driver.getName())
-            self.q_to_t[i] = link.getIndex()
+            self.q_to_t.append(link.getIndex())
 
             print "Driver ", i, ": ", driver.getName()
-            type, num = driver.getName().split('_')
+            try:
+                prefix, driver_name = driver.getName().split(':')
+            except ValueError:
+                driver_name = driver.getName()
+            type, num = driver_name.split('_')
+            num = int(num)
             if type in ['proximal', 'distal']:
                 self.u_to_n.append(i)
                 self.u_to_l.append(link.getID())
                 u_id = len(self.u_to_n) - 1
                 self.n_to_u[i] = u_id
+                if not self.hand.has_key(num):
+                    self.hand[num-1] = dict()
+                    self.hand[num-1]['f_to_u'] = np.array(self.u_dofs_per_finger*[0.0])
+                    if type is 'proximal':
+                        type_to_id = 0
+                    else:
+                        type_to_id = 1
+                self.hand[num-1]['f_to_u'][type_to_id] = u_id
             elif type == 'wire':
                 self.a_to_n.append(i)
                 a_id = len(self.a_to_n) - 1
                 self.n_to_a[i] = a_id
             elif type == 'swivel':
-                self.a_to_d.append(i)
-                d_id = len(self.a_to_d) -1
-                self.d_to_n[i] = d_id
+                self.d_to_n.append(i)
+                d_id = len(self.d_to_n) -1
+                self.n_to_d[i] = d_id
 
             self.l_to_i[link.getID()] = link.getIndex()
 
@@ -144,36 +168,35 @@ class HandEmulator(ActuatorEmulator):
         # checking load is successful
         assert len(self.u_to_n) == self.u_dofs
         self.u_dofs = len(self.u_to_n)
-        assert len(self.m_to_n) == self.m_fofs
+        assert len(self.m_to_n) == self.m_dofs
         self.m_dofs = len(self.m_to_n)
         assert len(self.a_to_n) == self.a_dofs
         self.a_dofs = len(self.a_to_n)
+
+        self.m_to_u = self.m_dofs * [-1]
 
         # loading elasticity and reduction map
         self.R = np.zeros((self.a_dofs, self.u_dofs))
         self.E = np.eye(self.u_dofs)
 
         self.q_a_ref = np.array(self.a_dofs*[0.0])
-        self.q_d_ref = np.array(self.r_dofs*[0.0])
+        self.q_d_ref = np.array(self.d_dofs*[0.0])
 
-        for i in xrange(driver_offset, self.robot.numDrivers()):
-            driver = self.robot.driver(i)
-            try:
-                _, _, finger, phalanx, fake_id = driver.getName().split('_')
-            except ValueError:
-                prefix, name = driver.getName().split(':')
-                _, _, finger, phalanx, fake_id = name.split('_')
-            u_id = self.n_to_u[i]
-            if u_id != -1:
-                joint_position = self.paramsLoader.phalanxToJoint(finger,phalanx)
-                self.R[u_id] = self.paramsLoader.handParameters[finger][joint_position]['r']
-                self.E[u_id,u_id] = self.paramsLoader.handParameters[finger][joint_position]['e']
+
+        # params loading
+        self.E[0, 0] = self.E[2, 2] = self.E[4, 4] = 0.1
+        self.E[1, 1] = self.E[3, 3] = self.E[5, 5] = 1
+
+        q = np.array(self.controller.getSensedConfig())
+        q = q[self.q_to_t]
+        q_u = q[self.u_to_n]
+        self.updateR(q_u)
 
         print 'Reflex Hand loaded.'
         print 'Actuated Joint Indices:', self.d_to_n
         print 'Synergy Joint Indices:', self.a_to_n
         print 'Underactuated Joint Indices:', self.u_to_n
-        print 'Joint parameters:', self.paramsLoader.handParameters
+        print 'Joint to Driver map:', self.q_to_t
         print 'R:', self.R
         #self.E = 20 * self.E
         print 'E:', self.E
@@ -215,20 +238,23 @@ class HandEmulator(ActuatorEmulator):
         q_u = q[self.u_to_n]
         q_m = q[self.m_to_n]
 
-        R_E_inv_R_T_inv = 1.0 / (self.R.dot(np.linalg.inv(self.E)).dot(self.R.T))
+        # updates self.R
+        self.updateR(q_u)
+
+        R_E_inv_R_T_inv = np.linalg.inv(self.R.dot(np.linalg.inv(self.E)).dot(self.R.T))
         sigma = q_a # q_a goes from 0.0 to 1.0
         f_c, J_c = self.get_contact_forces_and_jacobians()
         tau_c = J_c.T.dot(f_c)
 
         # tendon tension
-        f_a = R_E_inv_R_T_inv * sigma * self.synergy_reduction + R_E_inv_R_T_inv * np.linalg.inv(self.E).dot(tau_c)
+        f_a = R_E_inv_R_T_inv.dot(sigma) * self.synergy_reduction + R_E_inv_R_T_inv.dot(self.R).dot(np.linalg.inv(self.E).dot(tau_c))
         # emulate the synergy PID, notice there is no integrator ATM working on q_a_int
         torque_a = self.K_p*(self.q_a_ref - q_a) \
                    + self.K_d*(0.0 - dq_a) \
                    + self.K_i*self.q_a_int \
                    - (f_a / self.synergy_reduction)
 
-        torque_u = self.R.T*f_a - self.E.dot(q_u)
+        torque_u = self.R.T.dot(f_a) - self.E.dot(q_u)
 
         torque_m = self.K_p_m*(q_u[self.m_to_u] - q_m) - self.K_d_m*dq_m
 
@@ -242,6 +268,51 @@ class HandEmulator(ActuatorEmulator):
         #print 'tau_u:', torque_u
 
         return torque
+
+    def updateR(self, q_u):
+        da_vinci_f_i = np.ndarray((1,self.u_dofs_per_finger))
+
+        # base pulley radius
+        r0 = 0.1
+        # proximal phalanx equivalent pulley radius
+        r1 = 0.01
+        # the distal link does not have a pulley
+        r2 = 0.0
+
+        a0 = 0.1
+        b0 = 0.1
+        l0 = 0.1
+
+        a1 = 0.1
+        b1 = 0.1
+        l1 = 0.1
+
+        a2 = 0.1
+        b2 = 0.1
+        l2 = 0.1
+
+
+        # for each finger
+        for i in xrange(self.n_fingers):
+            # from q to theta
+            theta_1_u_id = self.hand[i]['f_to_u'][0]
+            theta_2_u_id = self.hand[i]['f_to_u'][1]
+            theta_1 = q_u[theta_1_u_id]
+            theta_2 = q_u[theta_2_u_id]
+
+            # from Birglen et al, 2007, page 55
+            r = r2 - r1
+            a = l1 - a1 + a2*np.cos(theta_2) - b2*np.sin(theta_2)
+            b = -b1 + a2*np.sin(theta_2) + b2*np.cos(theta_2)
+            l = a**2 + b**2 - r**2
+            R1 = r1 + (b1*(r*b - a*l) - (l1-a1)*(a*r + b*l))/(a**2+b**2)
+
+            # from Birglen Transmission matrix to R
+            da_vinci_f_i[0,0] = -r0
+            da_vinci_f_i[0,1] = R1
+
+            self.R[i,i*2:i*2+2] = da_vinci_f_i
+        return self.R
 
     def get_contact_forces_and_jacobians(self):
         """
@@ -300,10 +371,11 @@ class HandEmulator(ActuatorEmulator):
 
 
     def setCommand(self, command):
-        self.q_a_ref = [max(min(v, 1), 0) for v in command]
+        self.q_a_ref = [max(min(v, 1), 0) for i, v in enumerate(command) if i < 3]
+        self.q_d_ref = [max(min(v, 1), 0) for i, v in enumerate(command) if i == 3]
 
     def getCommand(self):
-        return [self.q_a_ref]
+        return np.hstack([self.q_a_ref, self.q_d_ref])
 
     def process(self, commands ,dt):
         if commands:
@@ -318,12 +390,14 @@ class HandEmulator(ActuatorEmulator):
             if 'force' in commands:
                 pass
 
-        qdes = self.controller.getCommandedConfig()
+        qdes = np.array(self.controller.getCommandedConfig())
+        qdes[[self.q_to_t[d_id] for d_id in self.d_to_n]] = self.q_d_ref
         dqdes = self.controller.getCommandedVelocity()
         self.controller.setPIDCommand(qdes, dqdes, self.output())
         
     def substep(self, dt):
-        qdes = self.controller.getCommandedConfig()
+        qdes = np.array(self.controller.getCommandedConfig())
+        qdes[[self.q_to_t[d_id] for d_id in self.d_to_n]] = self.q_d_ref
         dqdes = self.controller.getCommandedVelocity()
         self.controller.setPIDCommand(qdes, dqdes, self.output())
 
@@ -351,21 +425,48 @@ class HandSimGLViewer(GLSimulationProgram):
 
     def print_help(self):
         GLSimulationProgram.print_help()
-        print "o/l: increase/decrease synergy command"
+        print "y/h: raise/lower finger 1 command"
+        print "u/j: raise/lower finger 2 command"
+        print "i/k: raise/lower finger 3 command"
+        print "o/l: raise/lower preshape command"
 
-    def keyboardfunc(self, c, x, y):
-        # Put your keyboard handler here
-        # the current example toggles simulation / movie mode
-        if c == 'o':
+    def keyboardfunc(self,c,x,y):
+        #Put your keyboard handler here
+        #the current example toggles simulation / movie mode
+        if c=='y':
             u = self.handsim.getCommand()
             u[0] += 0.1
             self.handsim.setCommand(u)
-        elif c == 'l':
+        elif c=='h':
             u = self.handsim.getCommand()
             u[0] -= 0.1
             self.handsim.setCommand(u)
+        elif c=='u':
+            u = self.handsim.getCommand()
+            u[1] += 0.1
+            self.handsim.setCommand(u)
+        elif c=='j':
+            u = self.handsim.getCommand()
+            u[1] -= 0.1
+            self.handsim.setCommand(u)
+        elif c=='i':
+            u = self.handsim.getCommand()
+            u[2] += 0.1
+            self.handsim.setCommand(u)
+        elif c=='k':
+            u = self.handsim.getCommand()
+            u[2] -= 0.1
+            self.handsim.setCommand(u)
+        elif c=='o':
+            u = self.handsim.getCommand()
+            u[3] += 0.1
+            self.handsim.setCommand(u)
+        elif c=='l':
+            u = self.handsim.getCommand()
+            u[3] -= 0.1
+            self.handsim.setCommand(u)
         else:
-            GLSimulationProgram.keyboardfunc(self, c, x, y)
+            GLSimulationProgram.keyboardfunc(self,c,x,y)
         glutPostRedisplay()
 
         
