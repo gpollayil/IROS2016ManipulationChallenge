00e59b761cdc451d75e711d1a974a84b072a7185
Alessio <alessio.rocchi@iit.it>
2016-07-28 10:23:46 +0200

    Initial commit of compliant hand emulator (still not functioning)

--- plugins/soft_hand.py
+++ plugins/soft_hand.py
@@ -1,9 +1,9 @@
-from klampt import se3,vectorops
 from klampt import *
 from klampt.glrobotprogram import *
-from klampt.simulation import ActuatorEmulator
 from loaders.soft_hand_loader import SoftHandLoader
+from actuators.CompliantHandEmulator import CompliantHandEmulator
 import numpy as np
+
 
 
 #The hardware name
@@ -38,58 +38,19 @@ commandMaximum = [1]
 commandMinimumVelocity = [-1]
 commandMaximumVelocity = [1]
 
-class HandEmulator(ActuatorEmulator):
+class HandEmulator(CompliantHandEmulator):
     """An simulation model for the SoftHand for use with SimpleSimulation"""
     def __init__(self, sim, robotindex=0, link_offset=0, driver_offset=0):
         global klampt_model_name, gripper_name
-        self.world = sim.world
-        self.sim = sim
-        self.sim.enableContactFeedbackAll()
-        self.controller = self.sim.controller(robotindex)
-        self.robot = self.world.robot(robotindex)
+        CompliantHandEmulator.__init__(self, sim, robotindex, link_offset, driver_offset, a_dofs=1, d_dofs=0)
+
         self.paramsLoader = SoftHandLoader(klampt_model_name)
 
-        self.link_offset = link_offset
-        self.driver_offset = driver_offset
-
-        self.hand = dict()
-        self.mimic = dict()
-        self.n_dofs = 0
-
-        self.K_p = 3.0
-        self.K_d = 0.03
-        self.K_i = 0.01
-        self.q_a_int = 0.0
-
-        self.K_p_m = 3.0
-        self.K_d_m = 0.03
-
-        self.synergy_reduction = 7.0  # convert cable tension into motor torque
 
         print "Loaded robot name is:", self.robot.getName()
         print "Number of Drivers:", self.robot.numDrivers()
-        if self.robot.getName() in [gripper_name, "temp"]:
-            self.n_dofs = self.robot.numDrivers()
-            self.a_dofs = 1
-            self.d_dofs = 0
-        else:
+        if self.robot.getName() not in [gripper_name, "temp"]:
             raise Exception('loaded robot is not a soft hand, rather %s'%self.robot.getName())
-
-        self.u_to_l = []    # will contain a map from underactuated joint id (excluding mimics) to child link id
-        self.l_to_i = dict()# will contain a map from link id (global) to link index (local)
-        self.u_to_n = []    # will contain a map from underactuated joint id (excluding mimics) to driver id
-        self.a_to_n = []    # will contain a map from actuated id to driver id
-        self.m_to_n = []    # will contain a map from mimic joints id to driver id
-
-        self.n_to_u = np.array(self.n_dofs*[-1])
-        self.n_to_m = np.array(self.n_dofs*[-1])
-        self.n_to_a = np.array(self.n_dofs*[-1])
-        # skip the fixed joints:
-        # - soft_hand_kuka_coupler
-        # - soft_hand_clamp
-        # - soft_hand_softhand_base
-        # - soft_hand_palm_link
-        self.q_to_t = np.array(range(4,self.n_dofs+4))
 
         # loading previously defined maps
         for i in xrange(driver_offset, self.robot.numDrivers()):
@@ -146,9 +107,6 @@ class HandEmulator(ActuatorEmulator):
         self.R = np.zeros((self.a_dofs, self.u_dofs))
         self.E = np.eye(self.u_dofs)
 
-        self.q_a_ref = np.array(self.a_dofs*[0.0])
-        self.q_d_ref = np.array(self.d_dofs*[0.0])
-
         for i in xrange(driver_offset, self.robot.numDrivers()):
             driver = self.robot.driver(i)
             try:
@@ -163,164 +121,10 @@ class HandEmulator(ActuatorEmulator):
                 self.E[u_id,u_id] = self.paramsLoader.handParameters[finger][joint_position]['e']
 
         print 'Soft Hand loaded.'
-        print 'Mimic Joint Indices:', self.mimic
-        print 'Underactuated Joint Indices:', self.hand
+        self.printHandInfo()
+        print 'Mimic Joint Info:', self.mimic
+        print 'Underactuated Joint Info:', self.hand
         print 'Joint parameters:', self.paramsLoader.handParameters
-        print 'R:', self.R
-        #self.E = 20 * self.E
-        print 'E:', self.E
-
-        kP, kI, kD = self.controller.getPIDGains()
-        for i in self.u_to_n:
-            kP[i] = 0.0
-            kI[i] = 0.0
-            kD[i] = 0.0
-
-        # we could use directy a PID here...
-        for i in self.m_to_n:
-            kP[i] = 0.0
-            kI[i] = 0.0
-            kD[i] = 0.0
-
-        # we could use directy a PID here...
-        for i in self.a_to_n:
-            kP[i] = 0.0
-            kI[i] = 0.0
-            kD[i] = 0.0
-
-        self.controller.setPIDGains(kP, kI, kD)
-
-    def output(self):
-        torque = np.array(self.n_dofs * [0.0])
-
-        q = np.array(self.controller.getSensedConfig())
-
-        q = q[self.q_to_t]
-        dq = np.array(self.controller.getSensedVelocity())
-        dq = dq[self.q_to_t]
-
-        dq_a = dq[self.a_to_n]
-        dq_u = dq[self.u_to_n]
-        dq_m = dq[self.m_to_n]
-
-        q_a = q[self.a_to_n]
-        q_u = q[self.u_to_n]
-        q_m = q[self.m_to_n]
-
-        R_E_inv_R_T_inv = np.linalg.inv(self.R.dot(np.linalg.inv(self.E)).dot(self.R.T))
-        sigma = q_a # q_a goes from 0.0 to 1.0
-        f_c, J_c = self.get_contact_forces_and_jacobians()
-        tau_c = J_c.T.dot(f_c)
-
-        # tendon tension
-        f_a = R_E_inv_R_T_inv.dot(sigma) * self.synergy_reduction + R_E_inv_R_T_inv.dot(self.R).dot(np.linalg.inv(self.E).dot(tau_c))
-        # emulate the synergy PID, notice there is no integrator ATM working on q_a_int
-        torque_a = self.K_p*(self.q_a_ref - q_a) \
-                   + self.K_d*(0.0 - dq_a) \
-                   + self.K_i*self.q_a_int \
-                   - (f_a / self.synergy_reduction)
-
-        torque_u = self.R.T.dot(f_a) - self.E.dot(q_u)
-
-        torque_m = self.K_p_m*(q_u[self.m_to_u] - q_m) - self.K_d_m*dq_m
-
-        torque[self.a_to_n] = torque_a
-        torque[self.u_to_n] = torque_u
-        torque[self.m_to_n] = torque_m
-
-        #print 'q_u:', q_u
-        #print 'q_a_ref-q_a:',self.q_a_ref-q_a
-        #print 'q_u-q_m:', q_u[self.m_to_u]-q_m
-        #print 'tau_u:', torque_u
-
-        return torque
-
-    def get_contact_forces_and_jacobians(self):
-        """
-        Returns a force contact vector 1x(6*n_contacts)
-        and a contact jacobian matrix 6*n_contactsxn.
-        Contact forces are considered to be applied at the link origin
-        """
-        n_contacts = 0 # one contact per link
-        if hasattr(self, 'world'):
-            maxid = self.world.numIDs()
-            J_l = dict()
-            f_l = dict()
-            t_l = dict()
-            for l_id in self.u_to_l:
-                l_index = self.l_to_i[l_id]
-                link_in_contact = self.robot.link(l_index)
-                contacts_per_link = 0
-                for j in xrange(maxid): # TODO compute just one contact per link
-                    contacts_l_id_j = len(self.sim.getContacts(l_id, j))
-                    contacts_per_link += contacts_l_id_j
-                    if contacts_l_id_j > 0:
-                        if not f_l.has_key(l_id):
-                            f_l[l_id] = self.sim.contactForce(l_id, j)
-                            t_l[l_id] = self.sim.contactTorque(l_id, j)
-                        f_l[l_id] = vectorops.add(f_l[l_id], self.sim.contactForce(l_id, j))
-                        t_l[l_id] = vectorops.add(t_l[l_id], self.sim.contactTorque(l_id, j))
-                        ### debugging ###
-                        #print "link", link_in_contact.getName(), """
-                        #      in contact with obj""", self.world.getName(j), """
-                        #      (""", len(self.sim.getContacts(l_id, j)), """
-                        #      contacts)\n f=""",self.sim.contactForce(l_id, j), """
-                        #      t=""", self.sim.contactTorque(l_id, j)
-
-                ### debugging ###
-                """
-                if contacts_per_link == 0:
-                    print "link", link_in_contact.getName(), "not in contact"
-                """
-                if contacts_per_link > 0:
-                    n_contacts += 1
-                    J_l[l_id] = np.array(link_in_contact.getJacobian(
-                                        (0, 0, 0)))
-                    #print J_l[l_id].shape
-        f_c = np.array(6 * n_contacts * [0.0])
-        J_c = np.zeros((6 * n_contacts, self.u_dofs))
-
-        for l_in_contact in xrange(len(J_l.keys())):
-            f_c[l_in_contact * 6:l_in_contact * 6 + 3
-                ] = f_l.values()[l_in_contact]
-            f_c[l_in_contact * 6 + 3:l_in_contact * 6 + 6
-                ] = t_l.values()[l_in_contact]
-            J_c[l_in_contact * 6:l_in_contact * 6 + 6,
-                :] = np.array(
-                    J_l.values()[l_in_contact])[:, self.u_to_n]
-        return (f_c, J_c)
-
-
-    def setCommand(self, command):
-        self.q_a_ref = [max(min(v, 1), 0) for v in command]
-
-    def getCommand(self):
-        return self.q_a_ref
-
-    def process(self, commands ,dt):
-        if commands:
-            if 'position' in commands:
-                self.setCommand(commands['position'])
-                del commands['position']
-            if 'qcmd' in commands:
-                self.setCommand(commands['qcmd'])
-                del commands['qcmd']
-            if 'speed' in commands:
-                pass
-            if 'force' in commands:
-                pass
-
-        qdes = self.controller.getCommandedConfig()
-        dqdes = self.controller.getCommandedVelocity()
-        self.controller.setPIDCommand(qdes, dqdes, self.output())
-        
-    def substep(self, dt):
-        qdes = self.controller.getCommandedConfig()
-        dqdes = self.controller.getCommandedVelocity()
-        self.controller.setPIDCommand(qdes, dqdes, self.output())
-
-    def drawGL(self):
-        pass
 
 
 class HandSimGLViewer(GLSimulationProgram):
